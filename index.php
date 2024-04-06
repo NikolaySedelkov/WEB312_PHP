05be2063b7c889c63b0576f2576c6ae89b11fa1e
Nikolay <sedelkov.nikolay@gmail.com>
2024-04-06 10:52:32 +0300

    Создание массивов

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
+<?php
+    // Пустой массив
+    $empty_arr = array();
+    // Заполненый массив
+    // $fulled_arr[0] = 1; $fulled_arr[1] = 2; ... $fulled_arr[4] = 5;
+    $fulled_arr = array(1, 2, 3, 4, 5);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
 </html>
