42807b8d134053fba60cac0e8e6568106b4ecc93
Nikolay <sedelkov.nikolay@gmail.com>
2024-04-06 11:17:17 +0300

    цикл for для пробегания по массиву

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
     // Пустой массив
-    $empty_arr = array();
+    $empty_arr = array(); // []
     // Заполненый массив
     // $fulled_arr[0] = 1; $fulled_arr[1] = 2; ... $fulled_arr[4] = 5;
-    $fulled_arr = array(1, 2, 3, 4, 5);
+    $fulled_arr = array(1, 2, 3, 4, 5); // [1, 2, 3, 4, 5]
+
+    $options = ["Москва", "Санкт-петербург", "Воронеж", "Ялта"];
+
+    $size_cities = count($options);
+    for($i = 0; $i < $size_cities; ++$i) {
+        $size_postfixs = count( $fulled_arr );
+        for($j = 0; $j < $size_cities; ++$j) {
+            array_push(
+                $options,
+                "$options[$i]-$fulled_arr[$j]"
+            );
+        }
+    }
 ?>
 
 
@@ -15,6 +28,13 @@
         <title>Document</title>
     </head>
     <body>
-        
+        <select>
+            <?php
+                $size_cities = count($options);
+                for($i = 0; $i < $size_cities; ++$i) {
+                    echo "<option>$options[$i]</option>";
+                }
+            ?>
+        </select>
     </body>
 </html>
