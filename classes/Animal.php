7d8582e4829ee8969e149fce5028e96d58ca0b74
Nikolay <sedelkov.nikolay@gmail.com>
2024-04-20 10:54:29 +0300

    класс Animal

---
+++ classes/Animal.php
@@ -0,0 +1,20 @@
+<?php
+
+// Создать класс животное, состоящий из полей, возраст, имя, вес
+
+// Название классов объявляют с большой буквы
+class Animal {
+    public string $name;    // Имя      - строка
+    public int $age;        // Возраст  - целое число
+    public float $weight;   // Вес      - дробное число
+
+    // конструктор - Инициализатор - инициализирует все поля класса
+    public function __construct(string $name, int $age, float $weight) {
+        // $name; - воспринимается как параметро функции, а не поля класса
+        // для восприятия как поле класса нужно добавить $this-> : $this->name 
+        $this->name     = $name;
+        $this->age      = $age;
+        $this->weight   = $weight;
+    }
+
+}
