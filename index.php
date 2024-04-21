935d5b9eb27877ffe209344b11b85374c2840625
Nikolay <sedelkov.nikolay@gmail.com>
2024-04-21 12:44:06 +0300

    Практическая

--- index.php
+++ index.php
@@ -5,7 +5,70 @@
     include('./classes/abstract examples/geometry/Square.php');
     include('./classes/abstract examples/geometry/Circle.php');
 
-    $shape = new Shape(100, 20);
+    include('./classes/Pratice21_04.php');
+
+    $phone1 = new Phone(
+        'iPhone 12',
+        999.99,
+        'Latest iPhone model',
+        'Apple',
+        'A14 Bionic',
+        6,
+        1,
+        256,
+        'iOS'
+    );
+     
+    $phone2 = new Phone(
+        'Galaxy S21',
+        899.99,
+        'Flagship Android smartphone',
+        'Samsung',
+        'Snapdragon 888',
+        8,
+        2,
+        128,
+        'Android'
+    );
+     
+    $phone3 = new Phone(
+        'Pixel 5',
+        699.99,
+        'Google Pixel series',
+        'Google',
+        'Snapdragon 765G',
+        8,
+        1,
+        128,
+        'Android'
+    );
+     
+    $monitor1 = new Monitor(
+        'ASUS ROG Strix XG27WQ',
+        599.99,
+        '27-inch gaming monitor',
+        'ASUS',
+        27,
+        144,
+        ['HDMI', 'DisplayPort']
+    );
+     
+    $monitor2 = new Monitor(
+        'Dell UltraSharp U2720Q',
+        799.99,
+        '27-inch 4K monitor',
+        'Dell',
+        27,
+        60,
+        ['HDMI', 'DisplayPort', 'USB-C']
+    );
+
+    $products = array(
+        $phone1, $phone2, $phone3,
+        $monitor1, $monitor2
+    );
+
+    //$shape = new Shape(100, 20);
     $s = new Square(1, 1, 10);
     $c = new Circle(2, 2, 5);
 
@@ -19,7 +82,14 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php echo $s->getX();?></title>
-
+    </head>
+    <body>
+        <?php
+            foreach($products as $item) {
+                echo $item->getProduct();
+            } 
+        ?>
+        <hr/>
         <table>
             <caption>
                 Animal
@@ -76,8 +146,5 @@
             </tr>
             
         </table>
-    </head>
-    <body>
-        
     </body>
 </html>
