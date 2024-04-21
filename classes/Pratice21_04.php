0c5e59f86b6b42883742cc5496e7f34825e2f504
Nikolay <sedelkov.nikolay@gmail.com>
2024-04-21 12:34:16 +0300

    Практическая | задание 4

--- classes/Pratice21_04.php
+++ classes/Pratice21_04.php
@@ -1,6 +1,6 @@
 <?php
 
-class Product
+abstract class Product
 {
     public string   $name;
     public float    $price;
@@ -16,6 +16,8 @@ class Product
         $this->brand = $brand;
         //$this->id = 11111;
     }
+
+    abstract function getProduct() : string;
 }
 
 class Phone extends Product
@@ -45,6 +47,28 @@ class Phone extends Product
         $this->hdd = $hdd;
         $this->os = $os;
     }
+
+    public function getProduct() : string {
+        return "
+            <table>
+                <caption>Телефон $this->brand</caption>
+                <tbody>
+                    <tr>
+                        <td>Название</td><td>$this->name</td>
+                    </tr>
+                    <tr>
+                        <td>Цена</td><td>$this->price</td>
+                    </tr>
+                    <tr>
+                        <td>Описание</td><td>$this->description</td>
+                    </tr>
+                    <tr>
+                        <td>ОС</td><td>$this->os</td>
+                    </tr>
+                </tbody>
+            </table>
+        ";
+    }
 }
 
 class Monitor extends Product
@@ -66,5 +90,30 @@ class Monitor extends Product
         $this->diagonal = $diagonal;
         $this->frequency = $frequency;
         $this->ports = $ports;
+    }
+
+    public function getProduct() : string {
+        return "
+            <table>
+                <caption>Монитор $this->brand</caption>
+                <tbody>
+                    <tr>
+                        <td>Название</td><td>$this->name</td>
+                    </tr>
+                    <tr>
+                        <td>Цена</td><td>$this->price</td>
+                    </tr>
+                    <tr>
+                        <td>Описание</td><td>$this->description</td>
+                    </tr>
+                    <tr>
+                        <td>Диагональ</td><td>$this->diagonal</td>
+                    </tr>
+                    <tr>
+                        <td>астота</td><td>$this->frequency</td>
+                    </tr>
+                </tbody>
+            </table>
+        ";
     }
 }
