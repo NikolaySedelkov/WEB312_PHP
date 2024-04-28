4630cc15f3f176628f3898380f86dcfa217ea3c0
Nikolay <sedelkov.nikolay@gmail.com>
2024-04-28 12:52:09 +0300

    Счётчик обновлений

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php 
-    include('./classes/Animal.php');
-    include('./classes/Tigr.php');
+    // Счётчик перезагрузок страницы
 
-    $animal = new Animal("барсик", 4, 15.7);
-    $tigr   = new Tigr("бобик", 20, 55.99, 112);
+    $times = 0;
+    // Проверяем есть ли что-то в хранилише
+    if(isset($_COOKIE['times'])) {
+        // если есть, то достаем и увеличиваем на 1 (т.к счётчи)
+        $times = (int)$_COOKIE['times'] + 1;
+    } 
+    // Перезапись
+    setcookie("times", $times);
+    
 ?>
 
 <!DOCTYPE html>
@@ -12,65 +18,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
-
-        <table>
-            <caption>
-                Animal
-            </caption>
-            <tr>
-                <td>Имя</td><td><?php echo $animal->name;?></td>
-            </tr>
-            <tr>
-                <td>Возраст</td><td><?php echo $animal->age;?></td>
-            </tr>
-            <tr>    
-                <td>Вес</td><td><?php echo $animal->weight;?></td>
-            </tr>
-            <tr>    
-                <td>ID</td><td><?php echo $animal->getId();?></td>
-            </tr>
-            <?php
-                $data = $animal->getDiseases();
-                $count_data = count($data);
-                echo "
-                    <tr>
-                        <td rolspan=$count_data>Болезни</td><td>{$data[0]}</td>
-                    </tr>
-                ";
-
-                for($i = 1; $i < count($data); ++$i) {
-                    echo "
-                    <tr>
-                        <td></td><td>{$data[$i]}</td>
-                    </tr>
-                ";
-                }
-            ?>
-        </table>
-
-        <table>
-            <caption>
-                Tigr
-            </caption>
-            <tr>
-                <td>Имя</td><td><?php echo $tigr->name;?></td>
-            </tr>
-            <tr>
-                <td>Возраст</td><td><?php echo $tigr->age;?></td>
-            </tr>
-            <tr>    
-                <td>Вес</td><td><?php echo $tigr->weight;?></td>
-            </tr>
-            <tr>    
-                <td>Скорость</td><td><?php echo $tigr->velocity;?></td>
-            </tr>
-            <tr>    
-                <td>ID</td><td><?php echo $tigr->getId();?></td>
-            </tr>
-            
-        </table>
     </head>
     <body>
-        
+        <b><i>Счётчик обновления страниц: <?php echo $times; ?> </i></b>
     </body>
 </html>
