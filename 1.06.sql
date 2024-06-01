06083c25d3b308dad334622ed3f4be525ac82506
NikolaySedelkov <89905491+NikolaySedelkov@users.noreply.github.com>
2024-06-01 13:01:34 +0300

    Add files via upload

--- 1.06.sql
+++ 1.06.sql
@@ -30,6 +30,7 @@ SELECT *
         min - поиск минимального
         sum - рассчёт суммы элемент столбца
         avg - рассчёт среднего значения элементов столбца
+        count - количества записей
     }
 */
 SELECT 
@@ -56,4 +57,108 @@ SELECT
     min(length) as `Минимальная продолжительность`,
     avg(length) as `Средняя продолжительность`
 	FROM film
-    GROUP BY rating;
+    GROUP BY rating
+-- HAVING ~ WHERE | HAVING - фильтры, накладываемые на результат группировки
+    HAVING `Средняя продолжительность` > 115;
+    
+    
+/*
+	SELECT
+    FROM
+    WHERE
+    ORDER BY
+    GROUP BY
+    HAVING
+*/
+
+
+-- Пример
+/*
+	Найти рейтинги среди фильмов, которые сняты на англиском языке, 
+    и такое чтоб минимальная продолжительность всех фильмов среди рейтинга была не меньше 45
+*/
+SELECT rating
+	FROM film
+    WHERE language_id = 1
+    GROUP BY rating
+    HAVING min(length) >= 45;
+    
+-- Найти id актеров, которые снимались более чем в 30 фильмах
+SELECT actor_id, 
+	count(film_id) as `Стаж` -- count(*)
+	FROM film_actor
+    GROUP BY actor_id
+    HAVING `Стаж` > 30;
+    
+-- Запрос к двум и более таблицам
+-- 		Подзапросы
+-- Найти имена актеров, которые снимались более чем в 30 фильмах
+SELECT last_name, first_name
+	FROM actor
+    WHERE actor_id in (
+		SELECT actor_id
+			FROM film_actor
+			GROUP BY actor_id
+			HAVING count(film_id) > 30
+    );
+    
+SELECT *
+	FROM actor
+    WHERE actor_id in (1, 10, 100);
+
+-- 		JOIN
+-- 	`table1` JOIN `table2` ON `table1`.`foregen_key` = `table2`.`primary_key` 
+SELECT 
+	country,
+    group_concat(city separator ', ') as `Список городов`
+	FROM (
+		city INNER JOIN country ON city.country_id = country.country_id
+    )
+    GROUP BY country;
+    
+    
+-- =========================================
+
+/*
+	UPDATE - обновление/изменение данных в таблице
+*/
+SELECT *
+	FROM actor;
+
+/*
+	UPDATE `table`
+		SET `field` = НОВОЕ_ЗНАЧЕНИЕ
+        WHERE ...
+	
+*/
+UPDATE actor
+	SET first_name = concat(first_name, 'D')
+    WHERE actor_id = 3;
+    
+SELECT *
+	FROM address;
+
+UPDATE address
+	SET phone = concat('+', phone)
+    WHERE phone LIKE '7%';
+
+-- плохие примеры, так как в них мы меняем значения внешних и первичных ключи
+UPDATE address
+	SET 
+		city_id = 999999,
+		address_id = -address_id
+    WHERE address_id > 0;
+
+SELECT *
+	FROM actor
+    WHERE actor_id = 30000;
+
+UPDATE film_actor
+	SET actor_id = 30000
+    WHERE actor_id = 5;
+
+UPDATE actor
+	SET actor_id = 30000
+    WHERE actor_id = 5;
+    
+    
