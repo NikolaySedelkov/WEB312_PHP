419904596023f27008e815108b3c8848fcfc9e07
Nikolay <sedelkov.nikolay@gmail.com>
2024-06-08 13:02:09 +0300

    1

--- blade/routes/web.php
+++ blade/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NavigationController;
+use App\Http\Controllers\PageSchool;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/events', [NavigationController::class, 'events']);
+Route::get('/school', [NavigationController::class, 'school']);
+//Route::get('/school', [PageSchool::class, 'index']);
+
+
